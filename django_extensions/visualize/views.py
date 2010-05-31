d17dfa03d05722d3c2dda243829cf55f427c4535
vitaily <ppr.vitaly@gmail.com>
2010-05-31 20:00:36 +0300

    making grapsh staff-required by default

--- django_extensions/visualize/views.py
+++ django_extensions/visualize/views.py
@@ -1,8 +1,10 @@
 from StringIO import StringIO
 from django import forms
 from django.http import HttpResponse
+from django.conf import settings
 from django.db.models.loading import get_apps
 from django.shortcuts import render_to_response
+from django.contrib.admin.views.decorators import staff_member_required
 from django.template.context import RequestContext
 from django_extensions.visualize import output
 from django_extensions.visualize.modelviz import generate_dot
@@ -37,3 +39,6 @@ def graph_models(request):
     if 'image' in request.GET:
         return _image(request)
     return _form(request)
+
+if not getattr(settings, 'EXTENSIONS_MODELVIZ_NO_ADMIN', False):
+    graph_models = staff_member_required(graph_models)
